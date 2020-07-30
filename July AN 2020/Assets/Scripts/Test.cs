f271ea4fc6d67fe41b724a6400666f43592b3883
Usman <usman@circuitstream.com>
2020-07-30 17:23:56 -0600

    Intial commit session 4

---
+++ July AN 2020/Assets/Scripts/Test.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    GameObject prefab;
+    float shootForce;
+    // Start is called before the first frame update
+    int Sum(int x, int y)
+    {
+        int result = x + y;
+        return result;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GameObject temp = Instantiate(prefab, transform.position, transform.rotation);
+        temp.GetComponent<Rigidbody>().AddForce(transform.up * shootForce);
+    }
+}
