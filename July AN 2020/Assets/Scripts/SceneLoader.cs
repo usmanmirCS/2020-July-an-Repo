22b51ff7ea82c313efd6294d02f0733deec69426
Usman <usman@circuitstream.com>
2020-08-13 18:08:20 -0600

    Session 8

---
+++ July AN 2020/Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    /*
+    public void LaunchARScene()
+    {
+        SceneManager.LoadScene(1, LoadSceneMode.Single);
+    }
+
+    public void LaunchVRScene()
+    {
+        SceneManager.LoadScene(2, LoadSceneMode.Single);
+    }
+    */
+    public void LaunchScene(int sceneNumber)
+    {
+        SceneManager.LoadScene(sceneNumber, LoadSceneMode.Single);
+    }
+}
