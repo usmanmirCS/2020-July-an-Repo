ef7a74d6c53e2f980aec4f7dc26a4f9b7f46f72e
Usman <usman@circuitstream.com>
2020-08-18 18:17:50 -0600

    session 9

---
+++ July AN 2020/Assets/Scripts/SimHand/ShotCounter.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShotCounter : MonoBehaviour
+{
+    public int m_numShots;
+    public Text m_countDisplay;
+
+    public void UpdateDisplay()
+    {
+        m_countDisplay.text = "Shots Fired:" + m_numShots;
+    }
+}
--- July AN 2020/Assets/Scripts/SimHand/SimShoot.cs
+++ July AN 2020/Assets/Scripts/SimHand/SimShoot.cs
@@ -8,6 +8,8 @@ public class SimShoot : MonoBehaviour
     public float m_shootForce;
     public Transform m_spawnPoint;
 
+    public ShotCounter m_scScript;
+
     void Update()
     {
         if(Input.GetKeyDown(KeyCode.Mouse0))
@@ -15,6 +17,9 @@ public class SimShoot : MonoBehaviour
             GameObject fireball = Instantiate(m_prefabFireball, m_spawnPoint.position, m_spawnPoint.rotation);
             fireball.GetComponent<Rigidbody>().AddForce(m_spawnPoint.forward * m_shootForce);
             Destroy(fireball, 5);
+
+            m_scScript.m_numShots++;
+            m_scScript.UpdateDisplay();
         }
     }
 }
