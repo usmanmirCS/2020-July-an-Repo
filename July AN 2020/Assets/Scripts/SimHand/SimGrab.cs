eec735860c6d1e64fbea47c2ca814ee58f46e5b2
Usman <usman@circuitstream.com>
2020-08-25 23:33:34 -0600

    Session 11

---
+++ July AN 2020/Assets/Scripts/SimHand/SimGrab.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimGrab : MonoBehaviour
+{
+    public Animator m_anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Mouse1))
+        {
+            m_anim.SetBool("isGrabbing", true);
+        }
+        else if(Input.GetKeyUp(KeyCode.Mouse1))
+        {
+            m_anim.SetBool("isGrabbing", false);
+        }
+    }
+}
---
+++ July AN 2020/Assets/Scripts/VR/VRGrab.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VRGrab : MonoBehaviour
+{
+    public string m_gripName;
+    private bool m_gripHeld;
+
+    public Animator m_anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetAxis(m_gripName) > 0.5f && m_gripHeld == false)
+        {
+            m_gripHeld = true;
+            m_anim.SetBool("isGrabbing", true);
+        }
+        else if(Input.GetAxis(m_gripName) < 0.5f && m_gripHeld == true)
+        {
+            m_gripHeld = false;
+            m_anim.SetBool("isGrabbing", false);
+        }
+    }
+}
