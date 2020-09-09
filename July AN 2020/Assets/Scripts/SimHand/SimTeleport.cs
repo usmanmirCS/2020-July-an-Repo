15c9779236a5f6e303a4ec5c82a0f46129b7be51
Usman <usman@circuitstream.com>
2020-09-08 19:58:11 -0600

    session 15

---
+++ July AN 2020/Assets/Scripts/SimHand/SimTeleport.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimTeleport : MonoBehaviour
+{
+    public LayerMask m_validLayers;
+    public LineRenderer m_myLine;
+
+    private Vector3 m_hitPoint;
+    private bool m_canTeleport;
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.T))
+        {
+            //Raycast
+            RaycastHit hit;
+            if(Physics.Raycast(transform.position, transform.forward, out hit, 25, m_validLayers))
+            {
+                m_hitPoint = hit.point;
+                m_myLine.SetPosition(0, transform.position);
+                m_myLine.SetPosition(1, m_hitPoint);
+
+                m_canTeleport = true;
+                m_myLine.enabled = true;
+            }
+            else
+            {
+                m_canTeleport = false;
+                m_myLine.enabled = false;
+            }
+        }
+        else if(Input.GetKeyUp(KeyCode.T) && m_canTeleport)
+        {
+            transform.position = m_hitPoint;
+            m_canTeleport = false;
+            m_myLine.enabled = false;
+        }
+    }
+}
---
+++ July AN 2020/Assets/Scripts/VR/VRTeleport.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VRTeleport : MonoBehaviour
+{
+    public string m_buttonName;
+    public Transform m_VRRoot;
+
+    public LayerMask m_validLayers;
+    public LineRenderer m_myLine;
+
+    private Vector3 m_hitPoint;
+    private bool m_canTeleport;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButton(m_buttonName))
+        {
+            //Raycast
+            RaycastHit hit;
+            if (Physics.Raycast(transform.position, transform.forward, out hit, 25, m_validLayers))
+            {
+                m_hitPoint = hit.point;
+                m_myLine.SetPosition(0, transform.position);
+                m_myLine.SetPosition(1, m_hitPoint);
+
+                m_canTeleport = true;
+                m_myLine.enabled = true;
+            }
+            else
+            {
+                m_canTeleport = false;
+                m_myLine.enabled = false;
+            }
+        }
+        else if (Input.GetButtonUp(m_buttonName) && m_canTeleport)
+        {
+            m_VRRoot.position = m_hitPoint;
+            m_canTeleport = false;
+            m_myLine.enabled = false;
+        }
+    }
+}
